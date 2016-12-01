f62d1db3faddf5e8a2ab2b8a0dbd68b28a72c55a
王印彬 <wangyinbin@beijing.local>
2016-12-01 15:32:02 +0800

    add dubbo

---
+++ pay-indonesia-access-launcher/src/main/java/com/grb/indonesia/access/rest/UserFacade.java
@@ -0,0 +1,25 @@
+package com.grb.indonesia.access.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.grb.indonesia.api.UserExporterService;
+import com.grb.indonesia.service.UserService;
+
+//@Service(version="1.0.0")
+@Service
+public class UserFacade implements UserExporterService{
+
+	@Autowired UserService uservice;
+	
+	@Override
+	public void echo() {
+		
+		try {
+			uservice.testAnnotationTransaction();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
---
+++ pay-indonesia-api/src/main/java/com/grb/indonesia/api/UserExporterService.java
@@ -0,0 +1,8 @@
+package com.grb.indonesia.api;
+
+
+public interface UserExporterService {
+
+	public void echo();
+	
+}
