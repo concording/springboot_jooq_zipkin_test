bf37973bc64b9c0d8b1f77ebefa399faa1b42975
王印彬 <wangyinbin@beijing.local>
2016-12-01 14:12:18 +0800

    add api

---
+++ pay-indonesia-api/src/main/java/com/grb/indonesia/api/EchoDao.java
@@ -0,0 +1,23 @@
+package com.grb.indonesia.api;
+//package com.grb.indonesia.dao;
+//
+//        import tk.mybatis.mapper.common.Mapper;
+//        import org.apache.ibatis.annotations.SelectProvider;
+//        import org.apache.ibatis.annotations.Select;
+//
+//import com.grb.indonesia.entity.EchoEntity;
+//import com.grb.indonesia.dao.provider.EchoProvider;
+//
+//        import java.util.List;
+//
+//public interface EchoDao extends Mapper<EchoEntity> {
+//
+//    @SelectProvider(type = EchoProvider.class, method = "selectById")
+//    EchoEntity selectById(EchoEntity echoEntity);
+//
+//    @Select("select * from echo_info")
+//    List<EchoEntity> findAll();
+//
+//
+//
+//}
